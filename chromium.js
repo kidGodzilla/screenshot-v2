77baa2c4c40be38b62e39427b9010fdd7933930d
kidGodzilla <futhey@gmail.com>
2019-11-08 17:54:40 +0900

    screenshots but DIY

--- chromium.js
+++ chromium.js
@@ -9,7 +9,10 @@ async function getScreenshot(url, type, quality, fullPage) {
     });
 
     const page = await browser.newPage();
-    await page.goto(url);
+    // await page.setViewport(viewport)
+    await page.goto(url, { timeout: 10000 });
+    await page.waitFor(4000);
+
     const file = await page.screenshot({ type,  quality, fullPage });
     await browser.close();
     return file;
