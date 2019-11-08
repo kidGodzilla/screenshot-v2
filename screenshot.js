a9fe3275d8d222ea774b8985ab35bf46590345ce
kidGodzilla <futhey@gmail.com>
2019-11-08 22:16:10 +0900

    cleanup

--- screenshot.js
+++ screenshot.js
@@ -1,29 +1,26 @@
 const md5 = require('md5');
 const { parse } = require('url');
 const request = require('request');
-const urlExists = require('url-exists-async-await');
 const querystring = require('querystring');
 const { getScreenshot } = require('./chromium');
+const urlExists = require('url-exists-async-await');
 const { getInt, getUrlFromPath, isValidUrl } = require('./validator');
 
-let { BUNNY_STORAGE_API_KEY } = process.env;
+let { BUNNY_STORAGE_API_KEY, TTL } = process.env;
+if (!TTL) TTL = 3 * 24 * 60 * 60 * 1000; // Three days
 const CDN_URL = 'https://sshots.b-cdn.net';
 const BUCKET_NAME = 'sshots';
 
-// console.log('Keyvar:', BUNNY_STORAGE_API_KEY.length);
-
-let TTL = 3 * 24 * 60 * 60 * 1000; // Three days
-
-// Put a file in the bunny CDN
+// Put a file in the Bunny CDN
 function putFile (bucket, fn, file, cb) {
     let url = 'https://storage.bunnycdn.com/' + bucket + fn;
     if (!file) file = '0';
 
     request({
-        method: 'PUT',
-        url: url,
         headers: { 'AccessKey': BUNNY_STORAGE_API_KEY },
-        body: file
+        method: 'PUT',
+        body: file,
+        url: url
     }, (error, response, body) => {
         if (cb && typeof cb == 'function') cb({
             status: response.statusCode,
@@ -36,11 +33,10 @@ function putFile (bucket, fn, file, cb) {
 module.exports = async function (req, res) {
     try {
         const { pathname = '/', query = {} } = parse(req.url, true);
-        const { type = 'png', quality, height, width, dpr, fullPage, wait } = query;
+        const { type = 'png', quality, height, width, dpr, fullPage, wait, cache } = query;
         const qstring = querystring.stringify(query);
         let url = getUrlFromPath(pathname);
-
-        var viewport = null;
+        let viewport = null;
 
         if (width || height || dpr) viewport = {
             width: parseInt(width, 10) || 960,
@@ -54,18 +50,21 @@ module.exports = async function (req, res) {
             res.statusCode = 400;
             res.setHeader('Content-Type', 'text/html');
             res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
+
         } else {
             // Append query string to url
             if (qstring) url += '?' + qstring;
 
             // Check for a CDN cache
-            var ts = Math.floor((+ new Date) / TTL); // TTL-unique slug
-            var requestMd5 = md5(url); // md5 of request string
-            var filename = requestMd5 + '-' + ts + '.' + type;
-            console.log('Expected cached filename:', filename);
+            const ts = Math.floor((+ new Date) / TTL); // TTL-unique slug
+            const requestMd5 = md5(url); // md5 of request string
+            const filename = requestMd5 + '-' + ts + '.' + type;
 
             // Check to see if the filename exists at -- CDN_URL + '/' + filename
-            var exists = await urlExists(CDN_URL + '/' + filename);
+            let exists = false;
+
+            if (cache !== '0' && cache !== 'false')
+                exists = await urlExists(CDN_URL + '/' + filename);
 
             if (exists) {
                 // Redirect to the image
